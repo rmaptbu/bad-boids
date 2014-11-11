3023546994cd1f85fdd0a57a2090d9855173d48f
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-11-11 16:28:54 +0000

    turned update_boids into object 'Boids', adjusted test to interface with object correctly

--- boids.py
+++ boids.py
@@ -12,62 +12,66 @@ import yaml
 #import config file
 config=yaml.load(open("config.yml"))
 
-#define initial conditions
-boid_x_positions=[]
-boid_y_positions=[]
-boid_x_velocities=[]
-boid_y_velocities=[]
-for i in range(config["boids_number"]):
-	boid_x_positions.append(random.uniform(*config["x_position_range"]))
-	boid_y_positions.append(random.uniform(*config["y_position_range"]))
-	boid_x_velocities.append(random.uniform(*config["x_velocity_range"]))
-	boid_y_velocities.append(random.uniform(*config["y_velocity_range"]))
-boid_data=(boid_x_positions,boid_y_positions,boid_x_velocities,boid_y_velocities)
+class Boids(object):
+	#define initial conditions
+	def __init__(self):
+		boid_x_positions=[]
+		boid_y_positions=[]
+		boid_x_velocities=[]
+		boid_y_velocities=[]
+		for i in range(config["boids_number"]):
+			boid_x_positions.append(random.uniform(*config["x_position_range"]))
+			boid_y_positions.append(random.uniform(*config["y_position_range"]))
+			boid_x_velocities.append(random.uniform(*config["x_velocity_range"]))
+			boid_y_velocities.append(random.uniform(*config["y_velocity_range"]))
+		self.x_positions=boid_x_positions
+		self.y_positions=boid_y_positions
+		self.x_velocities=boid_x_velocities
+		self.y_velocities=boid_y_velocities
+		self.flocking_coeff=config["flocking_coeff"]/config["boids_number"]
+		self.match_speed_coeff=config["match_speed_coeff"]/config["boids_number"]
 
-def update_boids(boid_data):
-	x_positions,y_positions,x_velocities,y_velocities=boid_data
-	flocking_coeff=config["flocking_coeff"]/config["boids_number"]
-	match_speed_coeff=config["match_speed_coeff"]/config["boids_number"]
+	def update(self):
+		for i in range(len(self.x_positions)):
+			for j in range(len(self.x_positions)):
+				#define relevant variables
+				distance_x=self.x_positions[j]-self.x_positions[i]
+				distance_y=self.y_positions[j]-self.y_positions[i]
+				distance_total_sq=distance_x**2+distance_y**2
+				speed_diff_x=self.x_velocities[j]-self.x_velocities[i]
+				speed_diff_y=self.y_velocities[j]-self.y_velocities[i]
 
-	for i in range(len(x_positions)):
-		for j in range(len(x_positions)):
-			#define relevant variables
-			distance_x=x_positions[j]-x_positions[i]
-			distance_y=y_positions[j]-y_positions[i]
-			distance_total_sq=distance_x**2+distance_y**2
-			speed_diff_x=x_velocities[j]-x_velocities[i]
-			speed_diff_y=y_velocities[j]-y_velocities[i]
-			
-			# Fly towards the middle
-			x_velocities[i]+=distance_x*flocking_coeff
-			y_velocities[i]+=distance_y*flocking_coeff
-			
-			# Fly away from nearby boids			
-			if distance_total_sq < config["dispersion_threshold"]:
-				x_velocities[i]-=distance_x
-				y_velocities[i]-=distance_y
-				
-			# Try to match speed with nearby boids
-			if distance_total_sq < config["match_speed_threshold"]:
-				x_velocities[i]+=speed_diff_x*match_speed_coeff
-				y_velocities[i]+=speed_diff_y*match_speed_coeff
-				
-	# Move according to velocities
-	for i in range(len(x_positions)):
-		x_positions[i]+=x_velocities[i]
-		y_positions[i]+=y_velocities[i]
+				# Fly towards the middle
+				self.x_velocities[i]+=distance_x*self.flocking_coeff
+				self.y_velocities[i]+=distance_y*self.flocking_coeff
 
+				# Fly away from nearby boids
+				if distance_total_sq < config["dispersion_threshold"]:
+					self.x_velocities[i]-=distance_x
+					self.y_velocities[i]-=distance_y
+
+				# Try to match speed with nearby boids
+				if distance_total_sq < config["match_speed_threshold"]:
+					self.x_velocities[i]+=speed_diff_x*self.match_speed_coeff
+					self.y_velocities[i]+=speed_diff_y*self.match_speed_coeff
+
+		# Move according to velocities
+		for i in range(len(self.x_positions)):
+			self.x_positions[i]+=self.x_velocities[i]
+			self.y_positions[i]+=self.y_velocities[i]
+
+boids=Boids()
 figure=plt.figure()
 axes=plt.axes(xlim=(config["plot_dimensions"]['x']), ylim=(config["plot_dimensions"]['y']))
-scatter=axes.scatter(boid_data[0],boid_data[1])
+scatter=axes.scatter(boids.x_positions,boids.y_positions)
 
 def animate(frame):
-   update_boids(boid_data)
-   scatter.set_offsets(zip(boid_data[0],boid_data[1]))
+   boids.update()
+   scatter.set_offsets(zip(boids.x_positions,boids.y_positions))
 
 
 anim = animation.FuncAnimation(figure, animate,
-                               frames=config["animation"]['frames'], 
+                               frames=config["animation"]['frames'],
 							   interval=config["animation"]['interval'])
 
 if __name__ == "__main__":
--- test_boids.py
+++ test_boids.py
@@ -1,13 +1,19 @@
-from boids import update_boids
+from boids import Boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
 
 def test_bad_boids_regression():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
-    update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before): 
-            assert_almost_equal(after_value,before_value,delta=0.01)
+	regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
+	x_positions, y_positions, x_velocities, y_velocities = regression_data["before"]
+	boids=Boids()
+	boids.x_positions=x_positions
+	boids.y_positions=y_positions
+	boids.x_velocities=x_velocities
+	boids.y_velocities=y_velocities
+	boids.update()
+	boid_data=boids.x_positions, boids.y_positions, boids.x_velocities, boids.y_velocities
+	for after,before in zip(regression_data["after"],boid_data):
+		for after_value,before_value in zip(after,before): 
+			assert_almost_equal(after_value,before_value,delta=0.01)
 	
