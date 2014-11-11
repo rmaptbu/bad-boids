867c30ebad78f2ea81dfdc02f5eb789ecde1a5d3
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-11-11 18:18:11 +0000

    changed iteration over all boid object, needed to add updates for output parameters

--- boids.py
+++ boids.py
@@ -56,30 +56,39 @@ class Boids(object):
 			self.y_velocities.append(boid.y_velocity)
 
 	def update(self):
-		for i in range(len(self.x_positions)):
-			for j in range(len(self.x_positions)):
+		for boid in self.boids:
+			for other in self.boids:
 				#define relevant variables
-				distance_x=self.x_positions[j]-self.x_positions[i]
-				distance_y=self.y_positions[j]-self.y_positions[i]
+				distance_x=other.x_position-boid.x_position
+				distance_y=other.y_position-boid.y_position
 				distance_total_sq=distance_x**2+distance_y**2
-				speed_diff_x=self.x_velocities[j]-self.x_velocities[i]
-				speed_diff_y=self.y_velocities[j]-self.y_velocities[i]
+				speed_diff_x=other.x_velocity-boid.x_velocity
+				speed_diff_y=other.y_velocity-boid.y_velocity
 
 				# Fly towards the middle
-				self.x_velocities[i]+=distance_x*self.flocking_coeff
-				self.y_velocities[i]+=distance_y*self.flocking_coeff
+				boid.x_velocity+=distance_x*self.flocking_coeff
+				boid.y_velocity+=distance_y*self.flocking_coeff
 
 				# Fly away from nearby boids
 				if distance_total_sq < config["dispersion_threshold"]:
-					self.x_velocities[i]-=distance_x
-					self.y_velocities[i]-=distance_y
+					boid.x_velocity-=distance_x
+					boid.y_velocity-=distance_y
 
 				# Try to match speed with nearby boids
 				if distance_total_sq < config["match_speed_threshold"]:
-					self.x_velocities[i]+=speed_diff_x*self.match_speed_coeff
-					self.y_velocities[i]+=speed_diff_y*self.match_speed_coeff
+					boid.x_velocity+=speed_diff_x*self.match_speed_coeff
+					boid.y_velocity+=speed_diff_y*self.match_speed_coeff
 
 		# Move according to velocities
-		for i in range(len(self.x_positions)):
-			self.x_positions[i]+=self.x_velocities[i]
-			self.y_positions[i]+=self.y_velocities[i]
+		self.x_positions=[]
+		self.y_positions=[]
+		self.x_velocities=[]
+		self.y_velocities=[]
+		for boid in self.boids:
+			boid.x_position+=boid.x_velocity
+			boid.y_position+=boid.y_velocity
+			#update output parameters
+			self.x_positions.append(boid.x_position)
+			self.y_positions.append(boid.y_position)
+			self.x_velocities.append(boid.x_velocity)
+			self.y_velocities.append(boid.y_velocity)
