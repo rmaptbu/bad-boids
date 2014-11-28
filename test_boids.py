7f83c695c606f8806e81c16f8bca00a11c898955
Thore Buecking <rmaptbu@ucl.ac.uk>
2014-11-28 15:05:48 +0000

    used array to update fly towards middle, updated regression test; timing: 13.4

--- test_boids.py
+++ test_boids.py
@@ -1,13 +1,14 @@
 from numpy_boids import update_boids
+import numpy as np
 from nose.tools import assert_almost_equal
 import os
 import yaml
 
 def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
+    boid_data=np.array(regression_data["before"])
     update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
+    for after,before in zip(np.array(regression_data["after"]),boid_data):
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.01)
 	
